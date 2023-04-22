dc2dad7c8b73dc014f5b591aafa29cfd4ba8eea5
lufz <lucasfrazzetta@gmail.com>
2023-04-22 11:36:16 -0300

    cositas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import FetchingData from './components/FetchingData/FetchingData';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavbarContainer from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import FetchingData from './components/FetchingData/FetchingData';
 // import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 // import NavbarContainer from './components/Navbar/NavbarContainer';
 
 function App() {
 	return (
-		<div className='App'>
-			{/* <NavbarContainer />
-			<ItemListContainer /> */}
-			<FetchingData />
-		</div>
+		<BrowserRouter>
+			{/* donde voy a armar mi sistema de rutas*/}
+			<Routes>
+				{/* donde voy a poner mis rutas individuales, las voy a englobar */}
+				<Route element={<NavbarContainer />}>
+					<Route path='/' element={<ItemListContainer />} /> {/*Ruta raiz*/}
+					<Route path='/login' element={<h1> Este es un login </h1>} />
+					{/*cada truta. Necesaita dos parámetreos, 'path' (donde va la ruta que se muestra en la barra de navegacion) y 'element' (donde va el componente que queremos mostrar)*/}
+					<Route path='/sign-up' element={<h1> Acá deberías registrarte </h1>} />
+					<Route path='*' element={<h1>404 - La ruta no existe</h1>} />
+					{/*ruta por defecto*/}
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,27 @@
 import CarWidgetContainer from '../CarWidget/CarWidgetContainer';
 import styles from './Navbar.module.css';
+import { Outlet, Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<div className={styles.navbarContainer}>
-			<img
-				className={styles.navbarLogo}
-				src='https://res.cloudinary.com/dgwuimxmz/image/upload/v1680217535/Pngtree_blue_denim_summer_shorts_5862003_syjlmn.png'
-				alt='logo'
-			/>
-			<ul className={styles.itemsContainer}>
-				<li className={styles.item}>Todos</li>
-				<li className={styles.item}>Shorts</li>
-				<li className={styles.item}>Bermudas</li>
-				<li className={styles.item}>Jeans</li>
-			</ul>
-			<CarWidgetContainer />
-		</div>
+		<>
+			<div className={styles.navbarContainer}>
+				<Link to='/'>
+					{/* en el 'to' le pongo la ruta a donde quiero que vaya*/}
+					<img className={styles.navbarLogo} src='https://res.cloudinary.com/dgwuimxmz/image/upload/v1680217535/Pngtree_blue_denim_summer_shorts_5862003_syjlmn.png' alt='logo' />
+				</Link>
+				<ul className={styles.itemsContainer}>
+					<Link to='/login'>
+						<li className={styles.item}>Todos</li>
+					</Link>
+					<li className={styles.item}>Shorts</li>
+					<li className={styles.item}>Bermudas</li>
+					<li className={styles.item}>Jeans</li>
+				</ul>
+				<CarWidgetContainer />
+			</div>
+			<Outlet /> {/* Darle acceso al wrapper a los children */}
+		</>
 	);
 };
 
